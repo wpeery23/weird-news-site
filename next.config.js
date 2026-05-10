8841d02322f0793cfe2ecc8b1d5721976b890038
Web Developer <web-developer@weirdnews.com>
2026-05-10 10:58:38 +0000

    fix: resolve vercel build failure by adding empty turbopack config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: {},
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push('better-sqlite3');
