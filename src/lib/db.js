38d5b3d9805765fa1bd13bc025032f079cc21691
Web Developer <web-developer@weirdnews.com>
2026-05-11 19:09:12 +0000

    fix: create data directory if it doesn't exist for sqlite

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,13 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 
-const dbPath = path.join(process.cwd(), 'data/weird-news.db');
+const dataDir = path.join(process.cwd(), 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
+
+const dbPath = path.join(dataDir, 'weird-news.db');
 const db = new Database(dbPath);
 
 db.exec(`
